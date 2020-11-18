72b0f4283b7acec4f7d982d52eaffc7618d56619
Mai Yia Xiong <maiyiax@gmail.com>
2020-11-18 09:53:05 -0600

    updated so index.js would create html after prompts

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const fs = require('fs');
 const Manager = require('./lib//Manager.js');
 const Engineer = require('./lib/Engineer.js');
 const Intern = require('./lib/Intern.js');
-const { generateManagerCard, generateEngineerCard, generateInternCard, generateHTML } = require('./src/generate-html.js');
+//const generatePage = require('./src/generate-html.js');
 
 // set empty arrays to hold objects
-let employeeArray = [];
+let managerArray = [];
+let engineerArray = [];
+let internArray = [];
 
 // WHEN I start the application
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 
 // function to prompt user
-function promptUser() {
+const promptUser = () => {
     return inquirer
         // prompt Manager questions first
         .prompt(
@@ -46,13 +48,13 @@ function promptUser() {
                     message: "What is the Manager's office number?"
                 }
             ])
-        
+
         .then(managerResponse => {
             // push manager information into the employee array
             const manager = new Manager(managerResponse.name, managerResponse.id, managerResponse.email, managerResponse.office);
-            
 
-            employeeArray.push(manager);
+
+            managerArray.push(manager);
 
         })
 
@@ -67,7 +69,7 @@ function promptUser() {
 
 
 // This function prompts the questions for Engineers and Interns
-function employeeQuestions() {
+const employeeQuestions = () => {
     return inquirer.prompt(
         [
             {
@@ -118,10 +120,10 @@ function employeeQuestions() {
                             }
                         }
                     }
-                ]).then(({github}) => {
+                ]).then(({ github }) => {
                     const engineer = new Engineer(name, id, email, github);
-                    
-                    employeeArray.push(engineer);
+
+                    engineerArray.push(engineer);
                 });
 
                 // if intern was selected
@@ -140,56 +142,222 @@ function employeeQuestions() {
                         }
                     }
                 ])
-                .then(({school}) => {
-                    const intern = new Intern(name, id, email, school);
+                    .then(({ school }) => {
+                        const intern = new Intern(name, id, email, school);
 
-                    employeeArray.push(intern);
-                });
+                        internArray.push(intern);
+                    });
             }
         })
         // check if the user wants to add another employee
-        .then(addEmployee);
+        .then(addEmployee)
 };
 
 // this function checks if user would like to add additional employees
-function addEmployee() {
+const addEmployee = () => {
     inquirer.prompt({
         type: 'confirm',
         name: 'add',
         message: 'Would you like to add another employee?',
         default: false
     })
-    .then(({ add }) => {
-        // if user chooses to add another employee, then prompt employee questions again
-        if (add) {
-            console.log(`
+        .then(({ add }) => {
+            // if user chooses to add another employee, then prompt employee questions again
+            if (add) {
+                console.log(`
             ====================
             Add Another Employee
             ====================
             `)
-            return employeeQuestions();
+                return employeeQuestions();
 
-        // if there are no more employees, generate the html
-        } else {
-            generateHTML();
-            writeToFile();
-        };
-    });
+                // if there are no more employees, generate the html
+            } else {
+                writeToFile();
+            };
+        });
 };
 
-// function to generate html file
-const writeToFile = data => {
-    fs.writeFile('./dist/index.html', data, err => {
-        if (err) throw err;
+// block of code to generate html starts===================================================//
 
-        console.log(`
+// function to generate html template
+// const generateHTML = () => {
+//     generateCards();
+
+//     `
+//         <!DOCTYPE html>
+//         <html lang="en">
+
+//         <head>
+//             <meta charset="UTF-8">
+//             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//             <title>My Team Profile</title>
+//             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+//         </head>
+
+//         <body>
+//             <header>
+//                 <h1 class="bg-danger text-white text-center m-auto">My Team</h1>
+//             </header>
+
+//             <div class='d-flex flex-wrap p-3 m-4'>
+//             ${generateCards}
+//             </div>
+//         </body>
+
+//         </html>
+
+//     `
+//     ;
+
+// };
+
+// function to generate employee cards
+
+let managerInfo = "";
+const generateManagerCard = () => {
+    // loop through manager array
+    for (let i = 0; i < managerArray.length; i++) {
+        managerInfo = `
+                <div class="col mb-4">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-header bg-primary text-light">
+                            <h5 class="card-title">${managerArray[i].name}</h5>
+                            <p class="card-text">Manager</p>
+                        </div>
+                        <div class="card-body bg-light">
+                            <div class="border rounded">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">ID: ${managerArray[i].id} #</li>
+                                    <li class="list-group-item">
+                                        Email:<a href="mailto:address@email.com" target="_blank" class="card-link"> ${managerArray[i].email}</a>
+                                    </li>
+                                    <li class="list-group-item">Office Number: ${managerArray[i].office}</li>
+                                </ul>                   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+    };
+    return managerInfo;
+};
+
+
+let engineerInfo = "";
+const generateEngineerCard = () => {
+    // loop through engineer array
+    for (let j = 0; j < engineerArray.length; j++) {
+        engineerInfo = `
+            <div class="col mb-4">
+
+                <div class="card" style="width: 18rem;">
+
+                    <div class="card-header bg-primary text-light">
+                        <h5 class="card-title">${engineerArray[j].name}</h5>
+                        <p class="card-text">Engineer</p>
+                    </div>
+
+                    <div class="card-body bg-light">
+                        <div class="border rounded">
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item">ID: ${engineerArray[j].id} #</li>
+                                <li class="list-group-item">
+                                    Email:<a href="mailto:address@email.com" class="card-link"> ${engineerArray[j].email}</a>
+                                </li>
+                                <li class="list-group-item">
+                                    Github:<a href="https://github.com/${engineerArray[j].github}" target="_blank" class="card-link"> ${engineerArray[j].github}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            `
+    };
+    return engineerInfo;
+};
+
+
+let internInfo = "";
+const generateInternCard = () => {
+    // loop through intern array
+    for (let k = 0; k < internArray.length; k++) {
+        internInfo = `
+                <div class="col mb-4">
+    
+                    <div class="card" style="width: 18rem;">
+    
+                        <div class="card-header bg-primary text-light">
+                            <h5 class="card-title">${internArray[k].name}</h5>
+                            <p class="card-text">Intern</p>
+                        </div>
+    
+                        <div class="card-body bg-light">
+                            <div class="border rounded">
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">ID: ${internArray[k].id} #</li>
+                                    <li class="list-group-item">
+                                        Email:<a href="mailto:address@email.com" class="card-link"> ${internArray[k].email}</a>
+                                    </li>
+                                    <li class="list-group-item">School: ${internArray[k].school}</li>
+                                </ul>
+                            </div>
+                        </div>
+    
+                    </div>
+    
+                </div>
+                `
+    };
+    return internInfo;
+};
+
+
+
+// end code block=========================================================================================//
+const writeToFile = () => {
+    fs.writeFile('./dist/index.html',
+        `
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Team Profile</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    </head>
+    
+    <body>
+        <header>
+            <h1 class="bg-danger text-white text-center m-auto p-4">My Team</h1>
+        </header>
+    
+        <div class='d-flex flex-wrap p-3 m-4'>
+        ${generateManagerCard()}
+        ${generateEngineerCard()}
+        ${generateInternCard()}
+        </div>
+    </body>
+    
+    </html>
+    
+`
+        , err => {
+            if (err) throw err;
+
+            console.log(`
         ========================
         HTML has been generated!
         ========================
-        `)
+        `);
+        });
 
-        console.log(employeeArray);
-    });
-};
+}
+
 
 promptUser();
--- src/generate-html.js
+++ src/generate-html.js
@@ -1,161 +1,133 @@
-// create a function to generate manager card
-const generateManagerCard = managerCardInfo => {
-    const {name, id, email, office} = managerCardInfo;
+// // create a function to generate manager card
+// const generateManagerCard = managerCardInfo => {
+//     const {name, id, email, office} = managerCardInfo;
 
-    return `
-    <div class="col mb-4">
-        <div class="card" style="width: 18rem;">
-            <div class="card-header bg-primary text-light">
-                <h5 class="card-title">${name}</h5>
-                <p class="card-text">Manager</p>
-            </div>
-            <div class="card-body bg-light">
-                <div class="border rounded">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ID: ${id} #</li>
-                        <li class="list-group-item">
-                            Email:<a href="mailto:address@email.com" target="_blank" class="card-link"> ${email}</a>
-                        </li>
-                        <li class="list-group-item">Office Number: ${office}</li>
-                    </ul>                   
-                </div>
-            </div>
-        </div>
-    </div>
-    `
-};
+//     return `
+//     <div class="col mb-4">
+//         <div class="card" style="width: 18rem;">
+//             <div class="card-header bg-primary text-light">
+//                 <h5 class="card-title">${name}</h5>
+//                 <p class="card-text">Manager</p>
+//             </div>
+//             <div class="card-body bg-light">
+//                 <div class="border rounded">
+//                     <ul class="list-group list-group-flush">
+//                         <li class="list-group-item">ID: ${id} #</li>
+//                         <li class="list-group-item">
+//                             Email:<a href="mailto:address@email.com" target="_blank" class="card-link"> ${email}</a>
+//                         </li>
+//                         <li class="list-group-item">Office Number: ${office}</li>
+//                     </ul>                   
+//                 </div>
+//             </div>
+//         </div>
+//     </div>
+//     `
+// };
 
-// create a function to generate engineer card
-const generateEngineerCard = engineerCardInfo => {
-    const {name, id, email, github} = engineerCardInfo;
+// // create a function to generate engineer card
+// const generateEngineerCard = engineerCardInfo => {
+//     const {name, id, email, github} = engineerCardInfo;
 
-    if (!engineerCardInfo) {
-        return '';
-    }
+//     if (!engineerCardInfo) {
+//         return '';
+//     }
 
-    return `
-    <div class="col mb-4">
+//     return `
+//     <div class="col mb-4">
 
-        <div class="card" style="width: 18rem;">
+//         <div class="card" style="width: 18rem;">
 
-            <div class="card-header bg-primary text-light">
-                <h5 class="card-title">${name}</h5>
-                <p class="card-text">Engineer</p>
-            </div>
+//             <div class="card-header bg-primary text-light">
+//                 <h5 class="card-title">${name}</h5>
+//                 <p class="card-text">Engineer</p>
+//             </div>
 
-            <div class="card-body bg-light">
-                <div class="border rounded">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ID: ${id} #</li>
-                        <li class="list-group-item">
-                            Email:<a href="mailto:address@email.com" class="card-link"> ${email}</a>
-                        </li>
-                        <li class="list-group-item">
-                            Github:<a href="#" target="_blank" class="card-link"> ${github}</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+//             <div class="card-body bg-light">
+//                 <div class="border rounded">
+//                     <ul class="list-group list-group-flush">
+//                         <li class="list-group-item">ID: ${id} #</li>
+//                         <li class="list-group-item">
+//                             Email:<a href="mailto:address@email.com" class="card-link"> ${email}</a>
+//                         </li>
+//                         <li class="list-group-item">
+//                             Github:<a href="#" target="_blank" class="card-link"> ${github}</a>
+//                         </li>
+//                     </ul>
+//                 </div>
+//             </div>
 
-        </div>
+//         </div>
 
-    </div>
+//     </div>
 
-    `
-}
-// create a function to generate intern card
-const generateInternCard = internCardInfo => {
-    const {name, id, email, school} = internCardInfo;
+//     `
+// }
+// // create a function to generate intern card
+// const generateInternCard = internCardInfo => {
+//     const {name, id, email, school} = internCardInfo;
 
-    if (!internCardInfo) {
-        return '';
-    }
+//     if (!internCardInfo) {
+//         return '';
+//     }
 
-    return `
-    <div class="col mb-4">
+//     return `
+//     <div class="col mb-4">
 
-        <div class="card" style="width: 18rem;">
+//         <div class="card" style="width: 18rem;">
 
-            <div class="card-header bg-primary text-light">
-                <h5 class="card-title">${name}</h5>
-                <p class="card-text">Intern</p>
-            </div>
+//             <div class="card-header bg-primary text-light">
+//                 <h5 class="card-title">${name}</h5>
+//                 <p class="card-text">Intern</p>
+//             </div>
 
-            <div class="card-body bg-light">
-                <div class="border rounded">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">ID: ${id} #</li>
-                        <li class="list-group-item">
-                            Email:<a href="mailto:address@email.com" class="card-link"> ${email}</a>
-                        </li>
-                        <li class="list-group-item">School: ${school}</li>
-                    </ul>
-                </div>
-            </div>
+//             <div class="card-body bg-light">
+//                 <div class="border rounded">
+//                     <ul class="list-group list-group-flush">
+//                         <li class="list-group-item">ID: ${id} #</li>
+//                         <li class="list-group-item">
+//                             Email:<a href="mailto:address@email.com" class="card-link"> ${email}</a>
+//                         </li>
+//                         <li class="list-group-item">School: ${school}</li>
+//                     </ul>
+//                 </div>
+//             </div>
 
-        </div>
+//         </div>
 
-    </div>
-    `
-}
-
-// generate employee cards
-const generateEmployeeCards = employeeInfo => {
-    if (employeeInfo.Manager) {
-        return `
-        <div class="col mb-4">
-            <div class="card" style="width: 18rem;">
-                <div class="card-header bg-primary text-light">
-                    <h5 class="card-title">${employeeInfo.Manager.name}</h5>
-                    <p class="card-text">Manager</p>
-                </div>
-                <div class="card-body bg-light">
-                    <div class="border rounded">
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item">ID: ${employeeInfo.Manager.id} #</li>
-                            <li class="list-group-item">
-                                Email:<a href="mailto:address@email.com" target="_blank" class="card-link"> ${employeeInfo.Manager.email}</a>
-                            </li>
-                            <li class="list-group-item">Office Number: ${employeeInfo.Manager.office}</li>
-                        </ul>                   
-                    </div>
-                </div>
-            </div>
-        </div>
-        `
-    };
-}
-
-// function to generate html
-
-const generateHTML = () => {
-    return `
-        <!DOCTYPE html>
-        <html lang="en">
-
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>My Team Profile</title>
-            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-        </head>
-
-        <body>
-        <header>
-        <h1 class="bg-danger text-white text-center m-auto">My Team</h1>
-        </header>
-
-        <div class='d-flex flex-wrap p-3 m-4'>
-            ${generateManagerCard}
-            ${generateEngineerCard}
-            ${generateInternCard}
-        </div>
-        </body>
-
-        </html>
-    `
-}
+//     </div>
+//     `
+// }
 
 
 
-module.exports = { generateManagerCard, generateEngineerCard, generateInternCard, generateHTML };
+// module.exports = () => {
+// //const {name, id, email} = templateData;
+
+// return `
+//     <!DOCTYPE html>
+//     <html lang="en">
+
+//     <head>
+//         <meta charset="UTF-8">
+//         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//         <title>My Team Profile</title>
+//         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+//     </head>
+
+//     <body>
+//     <header>
+//     <h1 class="bg-danger text-white text-center m-auto">My Team</h1>
+//     </header>
+
+//     <div class='d-flex flex-wrap p-3 m-4'>
+//         ${generateManagerCard}
+//         ${generateEngineerCard}
+//         ${generateInternCard}
+//     </div>
+//     </body>
+
+//     </html>
+
+// `
+// }
